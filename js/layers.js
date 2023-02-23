59f462d8642cbc4669996c425379494916e343b3
teckentech <eliastrazzeri1@gmail.com>
2023-02-23 11:14:06 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -22,7 +22,9 @@ addLayer("p", {
     },
     upgrades: {
         11: {
-            
+            title: "Make this whatever you want!",
+            description: "Double your point gain.",
+            cost: new Decimal(1),
         },
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)

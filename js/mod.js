cd1e8127f2ff5213a14f019c47ba962199507291
teckentech <eliastrazzeri1@gmail.com>
2023-02-23 11:02:33 +0100

    cambio nome, id, autore

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "HyperStructure",
+	id: "5478923147891134789",
+	author: "Ontrekin",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 

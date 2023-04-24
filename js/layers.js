aeb7bc7c44967f89987aa7ec7abb05c2ee6ddd78
teckentech <eliastrazzeri1@gmail.com>
2023-04-24 13:41:19 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -22,9 +22,19 @@ addLayer("p", {
         return new Decimal(1)
     },
 
-midsection: [
-"grid"
-],
+    tabFormat: [
+        "main-display",
+        "prestige-button",
+        "blank",
+        "milestones",
+        "blank",
+        "upgrades",
+        "blank",
+        "buyables",
+        "blank",
+        "grid",
+        // ["p-proxy", ["p", grid]]
+    ],
     
 
     upgrades: {
@@ -57,19 +67,26 @@ midsection: [
 
     grid: {
         rows: 5, // If these are dynamic make sure to have a max value as well!
-        cols: 5,
+        cols: 1,
         
         getStartData(id) {
             if (id == 202) { 
-              return "ree"
+              return "empty"
             }
+            else {
+                return "empty"
+            }
+            
           },
         getUnlocked(id) { // Default
 var num
 var rowsA = this.rows
 var colsA = this.cols
+                difRows = (roswA-colsA)+1
+                difCols = (colsA-rowsA)+1
+
            for (var i = 1; i < colsA+1; i = i+1){
-                for (var j = 1; j < rowsA+2; j = j+1){
+                for (var j = 1; j < rowsA+1; j = j+1){
                 var num = (i*100)+j
             if (id == num)
             return true
@@ -80,36 +97,76 @@ var colsA = this.cols
 
         },
         onClick(data, id) { 
-            if(data != "empty"){
-                setGridData(this.layer, id, "empty")
-                }
-                else if(data == "empty"){
-                    var GridOb
+         //   if(data != "empty"){
+         //       setGridData(this.layer, id, "empty")
+         //       }
+         //       else if(data == "empty"){
+         //           var GridOb
     
-                    setGridData(this.layer, id, "empty")
-                }
+           //         setGridData(this.layer, id, "empty")
+            //    }
         },
         getDisplay(data, id) {
             return data 
         },
     },
 
-    grid2: {
-        rows: 1, // If these are dynamic make sure to have a max value as well!
-        cols: 6,
+
+    
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+})
+
+addLayer("p-proxy",{
+    name: "proxy", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "p", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1,
+    color: "#4BDC13",
+
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+
+    tabFormat: [
+        "main-display",
+        "prestige-button",
+        "blank",
+        "milestones",
+        "blank",
+        "upgrades",
+        "blank",
+        "buyables",
+        "blank",
+        "grid",
+        // ["p-proxy", ["p", grid]]
+    ],
+
+    grid: {
+        rows: 2, // If these are dynamic make sure to have a max value as well!
+        cols: 4,
         getStartData(id) {
-            return 0
+            if (id == 101) { 
+                return "empty"
+              }
+              else {
+                  return "empty"
+              }
         },
+        
         getUnlocked(id) { // Default
-            var num
-var rowsA = this.rows
-var colsA = this.cols
-           for (var i = 1; i < colsA+1; i = i+1){
-                for (var j = 1; j < rowsA+2; j = j+1){
+var rowsA2 = this.rows
+var colsA2 = this.cols
+           for (var i = 1; i < colsA2+1; i = i+1){
+                for (var j = 1; j < rowsA2+2; j = j+1){
                 var num = (i*100)+j
-            if (id == num)
+            if (id == (i*100)+j)
             return true
                 }
+                j = 1
             }
         },
         getCanClick(data, id) {
@@ -122,9 +179,6 @@ var colsA = this.cols
             return data 
         },
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    layerShown(){return true}
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+
 })
